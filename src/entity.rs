3f3d0b40713a62a2c5b91ef19db5aad518820a62
Alex T <aletapia@proton.me>
2025-01-12 13:14:57 -0700

    add entity module with Node and Client structs for serialization and deserialization

---
+++ src/entity.rs
@@ -0,0 +1,105 @@
+use serde::{
+    Deserialize, Deserializer, Serialize, Serializer,
+    de::{self, Visitor},
+};
+use std::fmt;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Node(usize);
+
+impl Serialize for Node {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let formatted = format!("n{}", self.0);
+        serializer.serialize_str(&formatted)
+    }
+}
+
+impl<'de> Deserialize<'de> for Node {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct NodeVisitor;
+
+        impl<'de> Visitor<'de> for NodeVisitor {
+            type Value = Node;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str(r#"a string in the format "n{int}"#)
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                match v.chars().next() {
+                    Some('n') => {
+                        let content = &v[1..];
+                        content.parse::<usize>().map(Node).map_err(|_| {
+                            de::Error::invalid_value(de::Unexpected::Str(v), &"a valid usize value")
+                        })
+                    }
+                    _ => Err(de::Error::invalid_value(
+                        de::Unexpected::Str(v),
+                        &r#"a string in the format "n{int}"#,
+                    )),
+                }
+            }
+        }
+
+        deserializer.deserialize_str(NodeVisitor)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Client(usize);
+
+impl Serialize for Client {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let formatted = format!("c{}", self.0);
+        serializer.serialize_str(&formatted)
+    }
+}
+
+impl<'de> Deserialize<'de> for Client {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct NodeVisitor;
+
+        impl<'de> Visitor<'de> for NodeVisitor {
+            type Value = Client;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str(r#"a string in the format "c{int}"#)
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                match v.chars().next() {
+                    Some('c') => {
+                        let content = &v[1..];
+                        content.parse::<usize>().map(Client).map_err(|_| {
+                            de::Error::invalid_value(de::Unexpected::Str(v), &"a valid usize value")
+                        })
+                    }
+                    _ => Err(de::Error::invalid_value(
+                        de::Unexpected::Str(v),
+                        &r#"a string in the format "c{int}"#,
+                    )),
+                }
+            }
+        }
+
+        deserializer.deserialize_str(NodeVisitor)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use tokio::io::{AsyncBufReadExt, BufReader, Lines, Stdin, stdin};
 
 pub mod client;
+pub mod entity;
 pub mod server;
 
 pub fn get_stdin_lines() -> Lines<BufReader<Stdin>> {
